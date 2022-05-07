7c5555d90d923c4548718394827cc097ac53e0ee
Helder Pereira <profhelder.pereira@fiap.com.br>
2022-05-07 14:51:07 -0300

    Simplicar setup do cloud9 usando functions

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,14 +1,20 @@
 
 #!/bin/bash
+
+# ==============================================================================================================================
+
+resize () {
+# Resizing para o disco local do ambiente:
+sh $HOME/environment/cloud9/scripts/resize.sh 20 > /dev/null
+}
+
+configure_deps () {
+
 test -d $HOME/environment/info || install -d -m 0700 -o ec2-user -g ec2-user $HOME/environment/info
 
 # Identificando o endereco pub da instancia:
 curl -s http://169.254.169.254/latest/meta-data/public-ipv4 -o $HOME/environment/info/PUBLIC_IP.txt && chown ec2-user: $HOME/environment/info/PUBLIC_IP.txt
 curl -s http://169.254.169.254/latest/meta-data/public-hostname -o $HOME/environment/info/PUBLIC_DNS.txt && chown ec2-user: $HOME/environment/info/PUBLIC_DNS.txt
-
-
-# Resizing para o disco local do ambiente:
-sh $HOME/environment/cloud9/scripts/resize.sh 20 > /dev/null
 
 # Instalação de componentes:
 printf "\n Configurando Dependencias \n"
@@ -37,6 +43,35 @@ pip install ansible boto3 boto
 printf "\n Gravando alterações no .bashrc \n"
 echo "export AWS_REGION=$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)" >> $HOME/.bashrc
 echo "export ANSIBLE_HOST_KEY_CHECKING=False" >> $HOME/.bashrc
+}
+
+configure_keys () {
+
+printf "Criando chave\n"
+
+test -f $HOME/environment/info/id_lab.pem || aws ec2 create-key-pair --key-name id_lab --key-type rsa | jq -r ".KeyMaterial" > $HOME/environment/info/id_lab.pem && chmod 600 $HOME/environment/info/id* | cp -p $HOME/environment/info/id_lab.pem $HOME/.ssh/id_rsa 
+test -f $HOME/environment/info/id_lab.pub || ssh-keygen -y -f $HOME/environment/info/id_lab.pem > $HOME/environment/info/id_lab.pub && chmod 600 $HOME/.ssh/id_rsa
+
+export PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+export PUBLIC_DNS=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
+
+printf "Identificando o SecurityGroup do projeto"
+aws ec2 describe-security-groups --filters Name=group-name,Values=*aws-cloud9* --query "SecurityGroups[*].[GroupName]" --output table
+
+# Definindo os SGs
+CURRENT_SG=$(aws ec2 describe-security-groups --filters Name=group-name,Values=*aws-cloud9* --query "SecurityGroups[*].[GroupName]" --output text)
+DEFAULT_SG=$(aws ec2 describe-security-groups --filters Name=group-name,Values=default --query "SecurityGroups[*].[GroupName]" --output text)
+
+# Regras para SSH e ICMP
+aws ec2 authorize-security-group-ingress --group-name $DEFAULT_SG --protocol tcp --port 0-65535 --source-group $CURRENT_SG
+aws ec2 authorize-security-group-ingress --group-name $DEFAULT_SG --protocol icmp --port -1 --source-group $CURRENT_SG    
+}
+
+# ==============================================================================================================================
+
+resize
+configure_deps
+configure_keys
 
 printf "\n Configurando Enviroment Env \n"
 bash
